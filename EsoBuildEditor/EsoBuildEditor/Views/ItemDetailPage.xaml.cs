88b86f8b1857432ba733d9c95ad9b468bc333200
davgor <davgor4@gmail.com>
2020-12-01 07:39:38 -0500

    Initial commit after project set up
    Initial commit of new build editor app

---
+++ EsoBuildEditor/EsoBuildEditor/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using EsoBuildEditor.ViewModels;
+
+namespace EsoBuildEditor.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
